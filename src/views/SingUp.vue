bc92745214098ddc2cdb7923b7a45d933d2625b7
Andsu <andersonrodrigues@glf.com.br>
2023-07-14 10:33:30 -0300

    firebase adicionado

---
+++ src/views/SingUp.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h1>Cadastre-se</h1>
+    <input type="email" placeholder="Email" v-model="email" />
+    <input type="password" placeholder="Senha" v-model="senha" />
+    <button @click="singup()">Cadastrar</button>
+  </section>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "SingUp",
+  data() {
+    return {
+      email: "",
+      senha: "",
+      initials: "",
+    };
+  },
+  methods: {
+    singup() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.senha)
+        .then(
+          (user) => {
+            console.log(user);
+          },
+          (err) => {
+            alert(err);
+          }
+        );
+    },
+  },
+};
+</script>
+
+<style></style>
